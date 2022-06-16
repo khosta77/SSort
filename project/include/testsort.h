e0c3b32a4d385336e5fd36c707fb143428a8bda2
khosta77 <puwerfulpants@gmail.com>
2022-06-16 21:13:25 +0300

    Начат тест

---
+++ project/include/testsort.h
@@ -0,0 +1,20 @@
+#ifndef SSORT_TESTSORT_H
+#define SSORT_TESTSORT_H
+
+#include <string>
+#include "stepansort.h"
+
+using namespace std;
+
+class testsort {
+public:
+    testsort() {
+
+    }
+private:
+    void test bubble(int *array, const int SIZE, string str) {
+        str = "Bubble";
+    }
+};
+
+#endif //SSORT_TESTSORT_H
